057703cae135c2ab876f62d74e985552cd706306
hennible0612 <sungwoo166@gmail.com>
2024-07-06 12:13:17 +0900

    .

--- src/main/java/hello/logtest/LogTestApplication.java
+++ src/main/java/hello/logtest/LogTestApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LogTestApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(LogTestApplication.class, args);
     }
 
