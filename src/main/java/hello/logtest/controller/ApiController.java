1c19bf38f1aaa25c3b64f4bd5431943a7c1215f5
hennible0612 <sungwoo166@gmail.com>
2024-07-14 20:11:00 +0900

    testest

--- src/main/java/hello/logtest/controller/ApiController.java
+++ src/main/java/hello/logtest/controller/ApiController.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,5 +50,10 @@ public class ApiController {
         } else {
             return ResponseEntity.ok("Solution is incorrect or failed to compile/run");
         }
+    }
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "success";
     }
 }
